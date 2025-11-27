31e8ae1e5c4c5bb3593908b8b75c07c05a34d3f8
alsi-lawr <alexlawrence1418@gmail.com>
2025-11-27 12:13:23 +0000

    feat(hm-modules): streamline home-manager modules

--- flake.nix
+++ flake.nix
@@ -7,34 +7,33 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { ... }:
-    let
-      hmModule = { config, pkgs, ... }: {
-        programs.neovim = {
-          enable = true;
-          defaultEditor = true;
-          viAlias = true;
-          vimAlias = true;
-        };
-
-        xdg.configFile."nvim".source = ./nvim;
-
-        home.packages = with pkgs; [
-          xclip
-          ripgrep
-          fd
-          lua-language-server
-          fzf
-          gcc
-          gnumake
-          pkg-config
-          cmake
-          ninja
-          unzip
-          tree-sitter
-          nodePackages_latest.neovim
-          python3Packages.pynvim
-        ];
+  outputs = { ... }: {
+    homeManagerModules.default = { config, pkgs, ... }: {
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
       };
-    in { homeManagerModules.default = hmModule; };
+
+      xdg.configFile."nvim".source = ./nvim;
+
+      home.packages = with pkgs; [
+        xclip
+        ripgrep
+        fd
+        lua-language-server
+        fzf
+        gcc
+        gnumake
+        pkg-config
+        cmake
+        ninja
+        unzip
+        tree-sitter
+        nodePackages_latest.neovim
+        python3Packages.pynvim
+      ];
+    };
+  };
 }
