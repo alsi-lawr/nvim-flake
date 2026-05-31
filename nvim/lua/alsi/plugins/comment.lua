dde008d9f0632befd1274cced15d0c3a9d1ee687
alsi-lawr <alexlawrence1418@gmail.com>
2026-05-31 16:27:20 +0100

    refactor(nvim): modernize plugin setup

--- nvim/lua/alsi/plugins/comment.lua
+++ nvim/lua/alsi/plugins/comment.lua
@@ -4,11 +4,14 @@ return {
     dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     config = function()
         local comment = require("Comment")
+        local comment_ft = require("Comment.ft")
 
-        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 
         comment.setup({
-            pre_hook = ts_context_commentstring.create_pre_hook(),
+            pre_hook = function(ctx)
+                return ts_context_commentstring(ctx) or comment_ft.get(vim.bo.filetype, ctx.ctype)
+            end,
         })
     end
 }
--- nvim/lua/alsi/plugins/treesitter.lua
+++ nvim/lua/alsi/plugins/treesitter.lua
@@ -1,89 +1,97 @@
+local languages = {
+	"json",
+	"javascript",
+	"typescript",
+	"tsx",
+	"yaml",
+	"html",
+	"css",
+	"markdown",
+	"markdown_inline",
+	"graphql",
+	"bash",
+	"lua",
+	"nix",
+	"vim",
+	"dockerfile",
+	"gitignore",
+	"query",
+	"vimdoc",
+	"c_sharp",
+	"fsharp",
+	"csv",
+	"diff",
+	"editorconfig",
+	"git_config",
+	"git_rebase",
+	"gitcommit",
+	"gitattributes",
+	"php",
+	"powershell",
+	"python",
+	"regex",
+	"ocaml",
+	"jinja2",
+}
+
+local function register_jinja_parser()
+	require("nvim-treesitter.parsers").jinja2 = {
+		install_info = {
+			url = "https://github.com/dbt-labs/tree-sitter-jinja2",
+			branch = "main",
+		},
+	}
+end
+
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = function()
+			register_jinja_parser()
+			require("nvim-treesitter").install(languages, { max_jobs = 4, summary = true }):wait(300000)
+		end,
+		config = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "TSUpdate",
+				callback = register_jinja_parser,
+			})
+
+			local treesitter = require("nvim-treesitter")
+
+			treesitter.setup()
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					if pcall(vim.treesitter.start, args.buf) then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
+			})
+
+			local ocaml = vim.api.nvim_create_augroup("ocaml", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				group = ocaml,
+				pattern = { "*.ml", "*.mli", "*.re", "*.rei" },
+				callback = function()
+					vim.bo.filetype = "ocaml"
+				end,
+			})
+
+			local jinja = vim.api.nvim_create_augroup("jinja", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+				group = jinja,
+				pattern = { "*.yaml.jinja" },
+				callback = function()
+					vim.bo.filetype = "jinja2"
+				end,
+			})
+		end,
 	},
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-
-		treesitter.setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"vimdoc",
-				"c_sharp",
-				"fsharp",
-				"csv",
-				"diff",
-				"editorconfig",
-				"git_config",
-				"git_rebase",
-				"gitcommit",
-				"gitattributes",
-				"gitignore",
-				"php",
-				"powershell",
-				"python",
-				"regex",
-				"ocaml",
-			},
-			incremental_selections = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-^[[C>",
-					node_incremental = "<C-^[[C>",
-					scope_incremental = false,
-					node_decremental = "<C-^[[D>",
-				},
-			},
-		})
-
-		local parsers = require("nvim-treesitter.parsers")
-		parsers.get_parser_configs().jinja2 = {
-			install_info = {
-				url = "https://github.com/dbt-labs/tree-sitter-jinja2",
-				files = { "src/parser.c" },
-				branch = "main",
-				generate_requires_npm = false,
-				requires_generate_from_grammar = false,
-			},
-			filetype = "jinja2",
-		}
-		local ocaml = vim.api.nvim_create_augroup("ocaml", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-			group = ocaml,
-			pattern = { "*.ml", "*.mli", "*.re", "*.rei" },
-			callback = function()
-				vim.bo.filetype = "ocaml"
-			end,
-		})
-
-		local jinja = vim.api.nvim_create_augroup("jinja", { clear = true })
-		vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-			group = jinja,
-			pattern = { "*.yaml.jinja" },
-			callback = function()
-				vim.bo.filetype = "jinja2"
-			end,
-		})
-	end,
+	{
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
 }
